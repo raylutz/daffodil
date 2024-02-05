909b2248d35c50b96f21b4509bec0298443eb07b
Raymond Lutz <raylutz@cognisys.com>
2024-02-04 21:53:18 -0800

    Update Pydf.py

--- Pydf/Pydf.py
+++ Pydf/Pydf.py
@@ -43,7 +43,11 @@ See README file at this location: https://github.com/raylutz/Pydf/blob/main/READ
             Changed the result of a row access, such as $d[$r, :$c] to return a list so it could be compatible with sum.
                 use pydf.select_irow() to select a row with dict as the result.
     
-    v0.2.0  (2024-02-03) Copied related code to Pydf repo and resolved all imports. All tests running.
+    v0.2.0  (2024-02-03) 
+            Copied related code to Pydf repo and resolved all imports. All tests running.
+            Added option of appending a plain list to pydf instance using .append()
+            Added 'omit_nulls' option to col(), col_to_la(), icol_to_la(), valuecounts_for_colname()
+            Added ability to groupby multiple cols
     
 """            
     
